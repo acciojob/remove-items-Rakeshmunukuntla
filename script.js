9b27a17c2f2a718d4b0cb901ce2fea707aa93a3d
Rakesh Munukuntla <130857014+Rakeshmunukuntla@users.noreply.github.com>
2024-11-30 10:04:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const colors=document.getElementById("colorSelect");
+const rem=document.getElementById("abc");
+
+rem.addEventListener("click",()=>{
+	const selectOptions=colors.options[colors.selectedIndex];
+	if(selectOptions)
+	{
+		colors.remove(colors.selectedIndex);
+	}
+})
